388401f8b36081b630c2e9d29db074e0a01853fd
Shubham Mittal <shubhammittal5114@gmail.com>
2019-07-11 04:36:27 +0530

    cpp_practice

--- pythagoras_triplet.cpp
+++ pythagoras_triplet.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 int main(){
 
-	long long n=0;
+	long long int n=0;
 	cin>>n;
-	long long a=0,b=0;
+	long long int a=0,b=0;
 
 	//LOGIC 
 
@@ -28,24 +28,26 @@ int main(){
 	}else{
 		if(n%2==0){
 
-			a = (((n*n)/4) - 1);
+			a = ((n*n)/4) - 1;
 
-			b = (((n*n)/4) + 1);
+			b = ((n*n)/4) + 1;
 
+			cout<<a<<" "<<b<<endl;
+			
 		}else if(n%2!=0){
 
-			a = (((n*n)-1)/2);
+			a = ((n*n)-1)/2;
+
+			b = ((n*n)+1)/2;
 			
-			b = (((n*n)+1)/2);
-		
+			cout<<a<<" "<<b<<endl;
+
 		}else{
 
 			cout<<-1<<endl;
 
 		}
 	}
-	
-	cout<<a<<" "<<b<<endl;
 
 	return 0;
 }
