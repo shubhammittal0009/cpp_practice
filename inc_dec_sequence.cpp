2af64f0863d5214fed8919e00a81ee64c9c91f45
Shubham Mittal <shubhammittal5114@gmail.com>
2019-07-11 01:20:40 +0530

    cpp_practice

--- inc_dec_sequence.cpp
+++ inc_dec_sequence.cpp
@@ -2,7 +2,36 @@
 using namespace std;
 int main(){
 
-	
-	
+	long n=0, s1[10000],s2[10000];
+	cin>>n;
+	int flag = 0;
+
+	int n1=0, n2=0;
+
+	cin>>n1;
+	s1[0]=n;
+
+	for(int i=1; i<n; i++){
+
+		cin>>n2;
+
+		if(n2<s1[i-1] && flag==0){
+			s1[i]=n2;
+			n1=n2;
+			s2[i-1]=n2;
+		}else if(n2>s2[i-1]){
+			flag = 1;
+			s2[i] = n2;
+		}else {
+			flag = 2;
+			break;
+		}
+	}
+
+	if(flag<2)
+		cout<<"true";
+	else
+		cout<<"false";
+
 	return 0;
 }
