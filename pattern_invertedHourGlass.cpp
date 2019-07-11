a9b88c9b48b33dc75f592d75cdb26e545842db5d
Shubham Mittal <shubhammittal5114@gmail.com>
2019-07-12 00:10:21 +0530

    cpp_practice

--- pattern_invertedHourGlass.cpp
+++ pattern_invertedHourGlass.cpp
@@ -6,12 +6,47 @@ int main(){
 	int n;
 	cin>>n;
 
-	for(int i=1; i<=(n+1)/2; i++){
+	for(int i=0; i<=n; i++){
 
-		
-		
-		cout<<endl;
+		for(int j=n; j>=(n-i); j--){
+			if(i==n)
+				continue;
+			cout<<j<<" ";
+		}
+
+		for(int j=(2*(n-i))-1; j>=1; j--){
+			if(i==n)
+				continue;
+			cout<<"  ";
+		}
+
+		for(int j=n-i; j<=n; j++){
+			if(i==n)
+				continue;
+			cout<<j<<" ";
+		}
+		if(i!=n)
+			cout<<endl;
 	}
+
+	for(int i=0; i<=n; i++){
+
+		for(int j=n; j>=i; j--){
+			cout<<j<<" ";
+		}
+
+		for(int j=1; j<=(2*i)-1; j++){
+			cout<<"  ";
+		}
+
+		for(int j=i; j<=n; j++){
+			if(j==0)
+				continue;
+			cout<<j<<" ";
+		}
+
+		cout<<endl;
+	}	
 
 	return 0;
 }
