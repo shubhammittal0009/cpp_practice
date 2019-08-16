2d2d180ac0688ce101a506f5b5dea629db6188ef
Shubham Mittal <shubhammittal5114@gmail.com>
2019-08-16 23:41:56 +0530

    functions

---
+++ fun.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n){
+  int res=1;
+  for(int i=1; i<=n; i++)
+    res *= i;
+  return res;
+}
+
+int nCr(int n, int r){
+  int nfact = fact(n);
+  int rfact = fact(r);
+  int nrfact = fact(n-r);
+  int result = ((nfact*rfact)/nrfact);
+  return result;
+}
+
+int main() {
+  int n=0;
+  int r=0;
+  cout<<"enter n and r for nCr: "<<endl;
+  cin>>n>>r;
+  int ans = nCr(n, r);
+  cout<<"Answer is: "<<ans;
+}
