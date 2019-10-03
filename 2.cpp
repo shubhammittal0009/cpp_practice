82ac306f75af56fd76c2c33c740ad1027ff3d10d
shubhammittal0009 <44203315+shubhammittal0009@users.noreply.github.com>
2019-10-04 00:26:47 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -5,19 +5,14 @@ int main(){
 	
 	int a,b;
 	int sum=0;
-
 	cin>>a;
 	cin>>b;
-
 	sum = (a+b);
-
 	cout<<"Sum is "<<sum;
-
 	if(sum%2==0){
-		cout<<"Sum is Even \n";
+		cout<<"Sum is Even number"<<endl;
 	}else{
-		cout<<"Sum is odd";
+		cout<<"Sum is odd odd number"<<endl;
 	}
-
 	return 0;
 }
